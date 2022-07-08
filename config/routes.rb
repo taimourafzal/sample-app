4bf8e332eaa99bd4fcf5b3861d672fea76821f06
taimourafzal <taimoorafzal@arkhitech.com>
2022-07-08 23:56:39 +0500

    new repo

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
+  get 'foo/bar'
+  get 'foo/baz'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  root "static_pages#home"
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,24 @@
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+# class ActiveSupport::TestCase
+#   # Run tests in parallel with specified workers
+#   parallelize(workers: :number_of_processors)
+
+#   # Set up all fixtures in test/fixtures/*.yml.
+#   fixtures :all
+
+#   # Add more helper methods to be used by all tests here...
+#   include ApplicationHelper
+# end
+
+# class ActiveSupport::TestCase
+#   fixtures :all
+#   # Returns true if a test user is logged in.
+#   def is_logged_in?
+#   !session[:user_id].nil?
+#   end
+#   end
