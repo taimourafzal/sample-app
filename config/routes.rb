104ed84266c1f9bf03023741b21cd55524ea060d
taimourafzal <taimoorafzal@arkhitech.com>
2022-07-09 21:15:31 +0500

    Implement advanced login

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  #delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy' 
 
   root "static_pages#home"
   resources :users
